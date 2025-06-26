85ee37ef76eb923dd15b5f3a2ad98910a5e115b0
ihabz <ihab.zeedia@gmail.com>
2025-06-26 10:31:12 -0700

    initial template

---
+++ OneSecIntegtation.php
@@ -0,0 +1,10 @@
+<?php
+namespace Stanford\OneSecIntegtation;
+
+class OneSecIntegtation extends \ExternalModules\AbstractExternalModule {
+    public function __construct() {
+        parent::__construct();
+        // Other code to run when object is instantiated
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# OneSec Integtation 
+
+The custom EM to Integration with the OneSec App during the consent process. 
